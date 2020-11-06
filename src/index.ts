615b5f951b233cea68a3c26138e3f716c5343ead
Adeniyi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2020-11-06 17:28:19 +0100

    ts clean

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { Cart, DolaObject } from "./types";
+import { Cart } from './types';
 
 const DolaBuyNow = (() => {
   // let DolaDataObject:DolaObject;
 
   const initialize = (key: string) => {
-    let merchantId = key
+    let merchantId = key;
     // DolaDataObject.merchantId = merchantId;
-    loadIframe(merchantId)
+    loadIframe(merchantId);
     return DolaBuyNow;
   }
 
@@ -16,34 +16,36 @@ const DolaBuyNow = (() => {
   }
 
   const showIframe = (cart: Cart) => {
-    const iframe  = document.getElementById("dolapayIframe") as HTMLIFrameElement;
+    const iframe  = document.getElementById('dolapayIframe') as HTMLIFrameElement;
 
     if (iframe) {
-      iframe.style.zIndex = "9999";
+      iframe.style.zIndex = '9999';
 
-      let dd = {cart}
-      console.log("+++++++++++++++++", dd)
-      iframe?.contentWindow && iframe?.contentWindow.postMessage(
-        dd,
-        process.env.CHECKOUT_APP_URL as string
-      );
+      let dd = {cart};
+      console.log('+++++++++++++++++', dd);
+      if (iframe?.contentWindow) {
+        return iframe?.contentWindow.postMessage(
+          dd,
+          process.env.CHECKOUT_APP_URL as string
+        );
+      }
     }
 	  
   }
 
   const loadIframe = (merchantId: string) => {
-    let dolaIframe:HTMLIFrameElement = document.createElement("iframe");
+    let dolaIframe:HTMLIFrameElement = document.createElement('iframe');
 
     dolaIframe.src = `${process.env.CHECKOUT_APP_URL}/${merchantId}`;
-    dolaIframe.style.width = "100%";
-    dolaIframe.style.height = "100%";
-    dolaIframe.style.border = "none";
-    // dolaIframe.loading = "lazy";
-    dolaIframe.id = "dolapayIframe";
-    dolaIframe.style.position = "fixed";
-    dolaIframe.style.top = "0";
-    dolaIframe.style.zIndex = "-9999";
-    dolaIframe.style.overflow= "hidden";
+    dolaIframe.style.width = '100%';
+    dolaIframe.style.height = '100%';
+    dolaIframe.style.border = 'none';
+    // dolaIframe.loading = 'lazy';
+    dolaIframe.id = 'dolapayIframe';
+    dolaIframe.style.position = 'fixed';
+    dolaIframe.style.top = '0';
+    dolaIframe.style.zIndex = '-9999';
+    dolaIframe.style.overflow= 'hidden';
 
     document.body.prepend(dolaIframe);
   }
