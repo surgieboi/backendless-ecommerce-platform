2d8fe49faed866d9a1514874bbd9b633e264d62b
Adeniyi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2020-12-15 17:33:47 +0100

    integrate custom data values

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,33 @@ import axios from 'axios';
 const DolaBuyNow = (() => {
   let tempId: string;
   const loadIframe = (merchantId: string) => {
-    let dolaIframe: IframeExtended = document.createElement('iframe');
+    try {
+      const dolaIframeExixting = document.getElementById('dolapayIframe');
 
-    dolaIframe.src = `https://dola-embedded-app-4f54yn4s1.vercel.app/${merchantId}`;
-    dolaIframe.style.width = '100%';
-    dolaIframe.style.height = '100%';
-    dolaIframe.style.border = 'none';
-    dolaIframe.loading = 'lazy';
-    dolaIframe.id = 'dolapayIframe';
-    dolaIframe.style.position = 'fixed';
-    dolaIframe.style.top = '0';
-    dolaIframe.style.zIndex = '-9999';
-    dolaIframe.style.overflow = 'hidden';
+      if (dolaIframeExixting) return;
+      let dolaIframe: IframeExtended = document.createElement('iframe');
 
-    document.body.prepend(dolaIframe);
-    return dolaIframe;
+      dolaIframe.src = `https://dev.checkout.dola.me/${merchantId}`;
+      dolaIframe.style.width = '100%';
+      dolaIframe.style.height = '100%';
+      dolaIframe.style.border = 'none';
+      dolaIframe.loading = 'lazy';
+      dolaIframe.id = 'dolapayIframe';
+      dolaIframe.style.position = 'fixed';
+      dolaIframe.style.top = '0';
+      dolaIframe.style.zIndex = '-9999';
+      dolaIframe.style.overflow = 'hidden';
+
+      document.body.prepend(dolaIframe);
+      return dolaIframe;
+    } catch (error) {
+      throw new Error(error.toString());
+    }
   };
 
   const attachCloseDolaEventListener = () => {
     window.addEventListener('message', async event => {
-      if (event.origin !== 'https://dola-embedded-app-4f54yn4s1.vercel.app')
-        return;
+      if (event.origin !== 'https://dev.checkout.dola.me') return;
       const target = document.getElementById('dolapayIframe');
 
       if (target && event.data['action'] === 'close-dola') {
@@ -50,8 +56,11 @@ const DolaBuyNow = (() => {
         const createIframe = loadIframe(id);
         if (createIframe) {
           attachCloseDolaEventListener();
-
+          setTimeout(() => {
+            attachActionToBuyNow(id);
+          });
           return {
+            id: id,
             attachDolaToCart: attachDolaToCart,
             attachDolaToItem: attachDolaToItem,
           };
@@ -63,6 +72,52 @@ const DolaBuyNow = (() => {
     } catch (error) {
       console.log(error.toString());
       return;
+    }
+  };
+
+  const attachActionToBuyNow = (id: string) => {
+    try {
+      const buyNowInstance = document.getElementById(id);
+
+      buyNowInstance?.addEventListener('click', () => {
+        const item = {
+          id: buyNowInstance?.dataset?.dolaId as string,
+          image: buyNowInstance?.dataset?.dolaImage as string,
+          quantity: 1,
+          title: buyNowInstance?.dataset?.dolaTitle as string,
+          price: parseInt(
+            buyNowInstance?.dataset?.dolaPrice as string,
+            10
+          ) as number,
+          grams: parseInt(
+            buyNowInstance?.dataset?.dolaWeight as string,
+            10
+          ) as number,
+          sku: buyNowInstance?.dataset?.dolaSku as string,
+          variantInfo: [],
+        };
+
+        console.log(
+          buyNowInstance,
+          id,
+          '--------------------------',
+          buyNowInstance?.dataset?.dolaWeight,
+          buyNowInstance?.dataset?.dolaPrice,
+          buyNowInstance?.dataset?.dolaTitle,
+          item
+        );
+
+        attachDolaToItem(
+          item,
+          parseInt(
+            buyNowInstance?.dataset?.dolaDiscount as string,
+            10
+          ) as number,
+          buyNowInstance?.dataset?.dolaCurrency as string
+        );
+      });
+    } catch (error) {
+      console.log(error.toString());
     }
   };
 
@@ -85,6 +140,8 @@ const DolaBuyNow = (() => {
       items: [item],
     };
 
+    console.log(buildSingleItemCart, '=====');
+
     showIframe(buildSingleItemCart);
   };
 
@@ -96,7 +153,7 @@ const DolaBuyNow = (() => {
     if (iframe && iframe.contentWindow) {
       iframe.contentWindow.postMessage(
         { cart, secret: `dola_${tempId}` },
-        'https://dola-embedded-app-4f54yn4s1.vercel.app' as string
+        'https://dev.checkout.dola.me' as string
       );
       iframe.style.zIndex = '9999';
     }
